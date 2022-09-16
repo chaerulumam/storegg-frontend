d57cc62e667ec2251c7fd27959f9673f5d88bd5c
Chaerul Umam <chaerulumamb244@gmail.com>
2022-09-16 22:49:32 +0700

    Memproses JWT dengan menggunakan Cookies

--- components/organisms/SignInForm/index.tsx
+++ components/organisms/SignInForm/index.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { setSignIn } from "../../../services/auth";
+import Cookies from "js-cookie";
 
 export default function SignInForm() {
   const [email, setEmail] = useState("");
@@ -25,7 +26,9 @@ export default function SignInForm() {
         toast.error(response.message);
       } else {
         toast.success("Login Berhasil");
-        router.push("/");
+        const { token } = response.data;
+        const tokenBase64 = btoa(token);
+        Cookies.set("token", tokenBase64, { expires: 1 });
       }
     }
   };
